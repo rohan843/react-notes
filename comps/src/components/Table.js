3a4bb61da68349a810bfe01359bb6afc93041e10
rohan843 <rohan.sharma2934@gmail.com>
2023-07-23 23:36:10 +0530

    create table reusable

--- comps/src/components/Table.js
+++ comps/src/components/Table.js
@@ -1,5 +1,30 @@
-function Table() {
-  return <div></div>;
+function Table({ data, config, keyFn }) {
+  const renderedColumns = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((val) => {
+    const renderedData = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(val)}
+        </td>
+      );
+    });
+    return (
+      <tr className="border-b" key={keyFn(val)}>
+        {renderedData}
+      </tr>
+    );
+  });
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedColumns}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
