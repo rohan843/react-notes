0e3065050bb4e67bd90494c09cc2062c7585b776
rohan843 <rohan.sharma2934@gmail.com>
2023-07-29 21:59:55 +0530

    add store

--- redux/cars/src/App.js
+++ redux/cars/src/App.js
@@ -1,5 +1,17 @@
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarValue from "./components/CarValue";
+import CarSearch from "./components/CarSearch";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <CarForm />
+      <CarList />
+      <CarValue />
+      <CarSearch />
+    </div>
+  );
 }
 
 export default App;
